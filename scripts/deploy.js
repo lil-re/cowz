3b7ba9b13b73705a849fbffa224cf99b4f14e523
Lilian <dev@lil.re>
2023-02-11 18:34:03 +0100

    Add and deploy smart contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,21 +5,18 @@
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 const hre = require("hardhat");
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const Cowz = await hre.ethers.getContractFactory("Cowz");
+  const cowz = await Cowz.deploy("Cowz", "COWZ", process.env.JSON_CID);
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
+  await cowz.deployed();
 
   console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    `Cowz deployed to ${cowz.address}`
   );
 }
 
